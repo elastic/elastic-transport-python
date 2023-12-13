e853a13ede375affdebc8c2c2a6abe81a4e50d52
Quentin Pradet <quentin.pradet@elastic.co>
2023-12-13 10:37:24 +0400

    Drop support for Python 3.6 (#109)

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ setup(
         "dataclasses; python_version<'3.7'",
         "importlib-metadata; python_version<'3.8'",
     ],
-    python_requires=">=3.6",
+    python_requires=">=3.7",
     extras_require={
         "develop": [
             "pytest",
@@ -78,7 +78,6 @@ setup(
         "Operating System :: OS Independent",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
