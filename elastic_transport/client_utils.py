d263c0dea6922d0688b77b20c504f153267036fb
Quentin Pradet <quentin.pradet@elastic.co>
2024-01-15 17:40:05 +0100

    Fix basic auth built from percent-encoded URLs (#143)
    * Fix basic auth built from percent-encoded URLs
    
    * Fix auth with colon is userinfo username

--- elastic_transport/client_utils.py
+++ elastic_transport/client_utils.py
@@ -19,6 +19,7 @@ import base64
 import binascii
 import dataclasses
 import re
+import urllib.parse
 from platform import python_version
 from typing import Optional, Tuple, TypeVar, Union
 from urllib.parse import quote as _quote
@@ -218,7 +219,13 @@ def url_to_node_config(
 
     headers = {}
     if parsed_url.auth:
-        username, _, password = parsed_url.auth.partition(":")
+        # `urllib3.util.url_parse` ensures `parsed_url` is correctly
+        # percent-encoded but does not percent-decode userinfo, so we have to
+        # do it ourselves to build the basic auth header correctly.
+        encoded_username, _, encoded_password = parsed_url.auth.partition(":")
+        username = urllib.parse.unquote(encoded_username)
+        password = urllib.parse.unquote(encoded_password)
+
         headers["authorization"] = basic_auth_to_header((username, password))
 
     host = parsed_url.host.strip("[]")
