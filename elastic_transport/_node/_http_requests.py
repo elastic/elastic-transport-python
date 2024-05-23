56d1e6832d0b438ee7cee3ab0f8dea6a14a89eb8
Quentin Pradet <quentin.pradet@elastic.co>
2024-05-23 11:59:42 +0400

    Fix lint

--- elastic_transport/_node/_http_requests.py
+++ elastic_transport/_node/_http_requests.py
@@ -170,14 +170,16 @@ class RequestsHttpNode(BaseNode):
         # Preload the HTTPConnectionPool so initialization issues
         # are raised here instead of in perform_request()
         if hasattr(adapter, "get_connection_with_tls_context"):
+            request = requests.Request(url=self.base_url)
+            prepared_request = self.session.prepare_request(request)
             adapter.get_connection_with_tls_context(
-                requests.Request(url=self.base_url), verify=self.session.verify
+                prepared_request, verify=self.session.verify
             )
         else:
             # elastic-transport is not vulnerable to CVE-2024-35195 because it uses
             # requests.Session and an SSLContext without using the verify parameter.
             # We should remove this branch when requiring requests 2.32 or later.
-            adapter.get_connection(self.base_url)  # type: ignore [no-untyped-call]
+            adapter.get_connection(self.base_url)
 
         self.session.mount(prefix=f"{self.scheme}://", adapter=adapter)
 
