4b45ab983f3ccf7a53debab75ee30f47a0a16c7c
Quentin Pradet <quentin.pradet@elastic.co>
2024-10-03 14:57:09 +0400

    Fix test_unsupported_tls_versions with HTTPX

--- setup.py
+++ setup.py
@@ -66,6 +66,8 @@ setup(
             "requests",
             "aiohttp",
             "httpx",
+            # https://github.com/encode/httpx/discussions/3214#discussioncomment-10830925
+            "httpcore<1.0.6",
             "respx",
             "opentelemetry-api",
             "opentelemetry-sdk",
